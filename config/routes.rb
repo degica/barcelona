8de6f28e034795a32f2dc11845025f24c73983a8
Kazunori Kajihiro <likerichie@gmail.com>
2015-09-24 15:19:35 +0900

    Do not shallow district routes
    We'll deploying a same heritage to different district. If heritage
    routes are shallowed, we need to include district ID in heritage JSON
    which is not useful

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :districts, except: [:new, :edit], shallow: true do
+  resources :districts, except: [:new, :edit] do
     resources :heritages, except: [:new, :edit] do
       post   :env_vars, on: :member, to: "heritages#set_env_vars"
       delete :env_vars, on: :member, to: "heritages#delete_env_vars"
