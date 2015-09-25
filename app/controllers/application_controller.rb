d97d4515cc87dce817ad52565b15e8b5072a79b1
Kazunori Kajihiro <likerichie@gmail.com>
2015-09-25 11:25:17 +0900

    Authenticate with token before every action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   # protect_from_forgery with: :exception
   protect_from_forgery with: :null_session
+  before_action :authenticate
+
+  def authenticate
+    @current_user = User.find_by_token(request.headers['HTTP_X_BARCELONA_TOKEN'])
+    raise "user not found" if @current_user.blank?
+  end
 end
