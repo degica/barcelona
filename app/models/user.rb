5217a723c8dd3b72062d3241bb0e5986cf15d2f0
Kazunori Kajihiro <likerichie@gmail.com>
2015-09-25 16:35:19 +0900

    Authorize with user team

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ActiveRecord::Base
-  ALLOWED_ORGS = ['degica']
+  ALLOWED_TEAM = {org: 'degica', team: 'developers'}
+
   attr_accessor :token
 
   def self.login!(github_token)
     client = Octokit::Client.new(access_token: github_token)
-    raise if (client.orgs.map(&:login) & ALLOWED_ORGS).blank?
+    raise unless allowed_user?(client.user_teams)
     user = User.find_or_create_by!(name: client.user.login)
     user.new_token!
     user
@@ -12,6 +13,12 @@ class User < ActiveRecord::Base
 
   def self.find_by_token(token)
     User.find_by(token_hash: Gibberish::SHA256(token))
+  end
+
+  def self.allowed_user?(user_teams)
+    user_teams.any? { |t|
+      t.name == ALLOWED_TEAM[:team] && t.organization.login == ALLOWED_TEAM[:org]
+    }
   end
 
   def new_token!
