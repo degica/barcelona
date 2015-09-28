e3fb1da4ece8b8837ddfe812cae6d8f659750282
Kazunori Kajihiro <likerichie@gmail.com>
2015-09-28 11:15:24 +0900

    Update district serializer

--- app/serializers/district_serializer.rb
+++ app/serializers/district_serializer.rb
@@ -1,5 +1,5 @@
 class DistrictSerializer < ActiveModel::Serializer
-  attributes :name
+  attributes :name, :vpc_id, :public_elb_security_group, :private_elb_security_group, :instance_security_group, :ecs_service_role, :ecs_instance_role, :private_hosted_zone_id
 
   has_many :heritages
 end
