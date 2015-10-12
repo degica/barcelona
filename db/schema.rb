f47d02875c00757bc52efd8c9ca2bdbf36e223d0
Kazunori Kajihiro <likerichie@gmail.com>
2015-10-12 14:43:49 +0900

    Add public_key to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151012052928) do
+ActiveRecord::Schema.define(version: 20151012054302) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -122,6 +122,7 @@ ActiveRecord::Schema.define(version: 20151012052928) do
     t.string   "token_hash"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text     "public_key"
   end
 
   add_index "users", ["name"], name: "index_users_on_name", unique: true, using: :btree
