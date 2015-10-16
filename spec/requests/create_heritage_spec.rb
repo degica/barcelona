0def22b9827f6552e9be9f5335e30106c85a7547
Kazunori Kajihiro <likerichie@gmail.com>
2015-10-16 10:19:59 +0900

    Add request specs

---
+++ spec/requests/create_heritage_spec.rb
@@ -0,0 +1,46 @@
+require 'rails_helper'
+
+describe "POST /districts/:district/heritages", :vcr, type: :request do
+  let(:user) { create :user }
+  let(:auth) { {"X-Barcelona-Token" => user.token} }
+  let(:district) { create :district }
+
+  before {Aws.config[:stub_responses] = true}
+
+  it "creates a heritage" do
+    params = {
+      name: "nginx",
+      image_name: "nginx",
+      image_tag: "latest",
+      before_deploy: "echo hello",
+      services: [
+        {
+          name: "web",
+          public: true,
+          cpu: 128,
+          memory: 256,
+          port_mappings: [
+            {
+              lb_port: 80,
+              container_port: 80
+            }
+          ]
+        }
+      ]
+    }
+    expect(DeployRunnerJob).to receive(:perform_later)
+    post "/districts/#{district.name}/heritages", params, auth
+    expect(response.status).to eq 200
+    heritage = JSON.load(response.body)["heritage"]
+    expect(heritage["name"]).to eq "nginx"
+    expect(heritage["image_name"]).to eq "nginx"
+    expect(heritage["image_tag"]).to eq "latest"
+    expect(heritage["before_deploy"]).to eq "echo hello"
+    expect(heritage["services"][0]["name"]).to eq "web"
+    expect(heritage["services"][0]["public"]).to eq true
+    expect(heritage["services"][0]["cpu"]).to eq 128
+    expect(heritage["services"][0]["memory"]).to eq 256
+    expect(heritage["services"][0]["port_mappings"][0]["lb_port"]).to eq 80
+    expect(heritage["services"][0]["port_mappings"][0]["container_port"]).to eq 80
+  end
+end
---
+++ spec/requests/destroy_heritage_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+describe "DELETE /heritages/:heritage", :vcr, type: :request do
+  let(:user) { create :user }
+  let(:auth) { {"X-Barcelona-Token" => user.token} }
+  let(:district) { create :district }
+
+  before {Aws.config[:stub_responses] = true}
+
+  it "updates a heritage" do
+    params = {
+      name: "nginx",
+      image_name: "nginx",
+      image_tag: "latest",
+      before_deploy: "echo hello",
+      services: [
+        {
+          name: "web",
+          public: true,
+          cpu: 128,
+          memory: 256,
+          port_mappings: [
+            {
+              lb_port: 80,
+              container_port: 80
+            }
+          ]
+        }
+      ]
+    }
+    post "/districts/#{district.name}/heritages", params, auth
+    expect(response).to be_success
+
+    delete "/heritages/nginx", nil, auth
+    expect(response.status).to eq 204
+  end
+end
