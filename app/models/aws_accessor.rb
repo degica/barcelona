9457fd36f2f8feef7a80fee66b5d9acb1176cdbb
Kazunori Kajihiro <likerichie@gmail.com>
2015-10-28 10:48:54 +0900

    Use environment variables by default

--- app/models/aws_accessor.rb
+++ app/models/aws_accessor.rb
@@ -27,6 +27,9 @@ class AwsAccessor
   private
 
   def credentials
-    Aws::Credentials.new(@access_key_id, @secret_access_key)
+    Aws::Credentials.new(
+      @access_key_id || ENV['AWS_ACCESS_KEY_ID'],
+      @secret_access_key || ENV['AWS_SECRET_ACCESS_KEY']
+    )
   end
 end
