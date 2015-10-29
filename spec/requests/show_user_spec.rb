46467cdccb03c404a1eddf8135a8cf06d3f2f181
Kazunori Kajihiro <likerichie@gmail.com>
2015-10-29 18:03:19 +0900

    Add users endpoint request specs

--- spec/requests/show_user_spec.rb
+++ spec/requests/show_user_spec.rb
@@ -20,3 +20,25 @@ describe "GET /user", type: :request do
     expect(body["roles"]).to eq ["developer"]
   end
 end
+
+describe "GET /users/:id", type: :request do
+  let(:auth) { {"X-Barcelona-Token" => user.token} }
+  let(:district) { create :district }
+  let(:user) { create :user, roles: ["developer"], districts: [district] }
+  let(:user2) { create :user, name: 'user2', roles: ["developer"], districts: [district] }
+
+  before do
+    allow_any_instance_of(District).to receive(:subnets) {
+      [double(subnet_id: 'subnet_id')]
+    }
+  end
+
+  it "shows user information" do
+    get "/users/#{user2.name}", nil, auth
+    expect(response.status).to eq 200
+    body = JSON.load(response.body)["user"]
+    expect(body["name"]).to eq user2.name
+    expect(body["districts"]).to eq [district.name]
+    expect(body["roles"]).to eq ["developer"]
+  end
+end
