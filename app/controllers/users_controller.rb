576f7b6af4ca837da83f42541e91810e8fcf112c
Kazunori Kajihiro <likerichie@gmail.com>
2015-10-29 18:02:40 +0900

    Add index users action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,8 +11,13 @@ class UsersController < ApplicationController
     end
   end
 
+  def index
+    users = User.all
+    render json: users
+  end
+
   def show
-    render json: current_user
+    render json: @user
   end
 
   def update
