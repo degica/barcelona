b6cc8abea9ff512564cffe69a51a4660a5735842
Kazunori Kajihiro <likerichie@gmail.com>
2015-11-14 10:46:18 -0800

    Add Datadog plugin

---
+++ lib/barcelona/plugins/datadog_plugin.rb
@@ -0,0 +1,50 @@
+module Barcelona
+  module Plugins
+    class DatadogPlugin < Base
+      def on_container_instance_user_data(instance, user_data)
+        user_data.run_commands += [
+          agent_command
+        ]
+
+        user_data
+      end
+
+      private
+
+      def agent_command
+        ["docker", "run", "-d",
+         "--name", "dd-agent",
+         "-h", "`hostname`",
+         "-v", "/var/run/docker.sock:/var/run/docker.sock",
+         "-v", "/proc/:/host/proc/:ro",
+         "-v", "/sys/fs/cgroup/:/host/sys/fs/cgroup:ro",
+         "-e", "API_KEY=#{api_key}",
+         proxy_env,
+         tags,
+         "datadog/docer-dd-agent:latest"
+        ].flatten.compact.join(" ")
+      end
+
+      def proxy_env
+        if proxy_plugin.present?
+          [
+            "-e", "PROXY_HOST=#{proxy_plugin.proxy_host}",
+            "-e", "PROXY_PORT=#{proxy_plugin.proxy_port}"
+          ]
+        end
+      end
+
+      def tags
+        "TAGS=\"barcelona,district:#{district.name}\""
+      end
+
+      def proxy_plugin
+        @proxy_plugin ||= district.plugins.find_by(name: "proxy").try(:plugin)
+      end
+
+      def api_key
+        attributes[:api_key]
+      end
+    end
+  end
+end
---
+++ spec/lib/barcelona/plugins/datadog_plugin_spec.rb
@@ -0,0 +1,51 @@
+require 'rails_helper'
+
+module Barcelona
+  module Plugins
+    describe DatadogPlugin do
+      context "without proxy plugin" do
+        let!(:district) do
+          create :district, plugins_attributes: [
+                   {
+                     name: 'datadog',
+                     plugin_attributes: {
+                       "api_key" => "abcdef"
+                     }
+                   }
+                 ]
+        end
+
+        it "gets hooked with container_instance_user_data trigger" do
+          section = district.sections[:private]
+          ci = ContainerInstance.new(section, instance_type: 't2.micro')
+          user_data = YAML.load(Base64.decode64(ci.instance_user_data))
+          expect(user_data["runcmd"].last).to eq "docker run -d --name dd-agent -h `hostname` -v /var/run/docker.sock:/var/run/docker.sock -v /proc/:/host/proc/:ro -v /sys/fs/cgroup/:/host/sys/fs/cgroup:ro -e API_KEY=abcdef TAGS=\"barcelona,district:dev\" datadog/docer-dd-agent:latest"
+        end
+      end
+
+      context "with proxy plugin" do
+        let!(:district) do
+          create :district, plugins_attributes: [
+                   {
+                     name: "proxy",
+                   },
+                   {
+                     name: 'datadog',
+                     plugin_attributes: {
+                       "api_key" => "abcdef"
+                     }
+                   }
+                 ]
+        end
+
+        it "gets hooked with container_instance_user_data trigger" do
+          section = district.sections[:private]
+          ci = ContainerInstance.new(section, instance_type: 't2.micro')
+          user_data = YAML.load(Base64.decode64(ci.instance_user_data))
+          expect(user_data["runcmd"].last).to include "-e PROXY_HOST=main.#{district.name}-proxy.bcn"
+          expect(user_data["runcmd"].last).to include "-e PROXY_PORT=3128"
+        end
+      end
+    end
+  end
+end
