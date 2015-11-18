65997ee01ee81a0a3eb2cf59ae3489c8926b340c
Kazunori Kajihiro <likerichie@gmail.com>
2015-11-18 12:48:18 +0900

    Fix broken random-host-port-assigning

--- app/models/port_mapping.rb
+++ app/models/port_mapping.rb
@@ -5,20 +5,21 @@ class PortMapping < ActiveRecord::Base
   validates :service, :lb_port, :container_port, presence: true
   validates :host_port, numericality: {greater_than: 1023, less_than: 20000}
   validates :protocol, inclusion: { in: %w(tcp udp) }
-  validate :validate_host_port_uniqueness_on_district
+  validate :validate_host_port_uniqueness_on_district, on: :create
 
   after_initialize do |mapping|
     mapping.protocol ||= "tcp"
   end
 
-  before_validation :assign_host_port, on: :create
+  before_validation :assign_random_host_port
 
   scope :tcp, -> { where(protocol: "tcp") }
   scope :udp, -> { where(protocol: "udp") }
 
   private
 
-  def assign_host_port
+  def assign_random_host_port
+    return if host_port.present?
     available_ports = RANDOM_HOST_PORT_RANGE.to_a - used_host_ports
     self.host_port = available_ports.sample
   end
@@ -34,5 +35,6 @@ class PortMapping < ActiveRecord::Base
                        .joins(service: { heritage: :district })
                        .where("heritages.district_id" => service.heritage.district.id)
                        .pluck(:host_port)
+                       .compact
   end
 end
