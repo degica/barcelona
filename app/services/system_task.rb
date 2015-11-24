9d99e3f004613d719ce7a1890a8256e745b1da42
Kazunori Kajihiro <likerichie@gmail.com>
2015-11-24 17:31:37 +0900

    Refactor UpdateUserTask; introduce SystemTask base class

---
+++ app/services/system_task.rb
@@ -0,0 +1,53 @@
+class SystemTask
+  attr_accessor :section, :user
+  delegate :aws, :district, to: :section
+
+  def initialize(section, user)
+    @section = section
+    @user = user
+  end
+
+  def run(container_instance_arns, env = {})
+    td = task_definition
+    aws.ecs.register_task_definition(task_definition)
+
+    resp = aws.ecs.start_task(
+      cluster: section.cluster_name,
+      task_definition: task_family,
+      overrides: {
+        container_overrides: [
+          {
+            name: task_family,
+            environment: env.map { |k, v| {name: k, value: v} }
+          }
+        ]
+      },
+      container_instances: container_instance_arns
+    )
+  end
+
+  def task_definition
+    {
+      family: task_family,
+      container_definitions: [container_definition],
+      volumes: volumes
+    }
+  end
+
+  def container_definition
+    district.base_task_definition.merge(
+      name: task_family,
+      cpu: 32,
+      memory: 32,
+      essential: true
+    )
+  end
+
+  def volumes
+    []
+  end
+
+  def task_family
+    raise NotImplementedError
+  end
+end
--- app/services/update_user_task.rb
+++ app/services/update_user_task.rb
@@ -1,70 +1,51 @@
-class UpdateUserTask
-  attr_accessor :section, :user
-  delegate :aws, to: :section
-
-  def initialize(section, user)
-    @section = section
-    @user = user
-  end
-
+class UpdateUserTask < SystemTask
   def run
     Rails.logger.info "Updating user #{user.name} for #{section.cluster_name}"
-    aws.ecs.register_task_definition(
-      family: "update_user",
-      container_definitions: [
+    env = {
+      "USER_NAME" => user.name,
+      "USER_GROUPS" => user.instance_groups.join(","),
+      "USER_PUBLIC_KEY" => user.public_key.presence,
+      "USER_DOCKERCFG" => section.dockercfg.try(:to_json)
+    }.compact
+
+    arns = section.container_instances.map{ |c| c[:container_instance_arn] }
+    super(arns, env)
+  end
+
+  def task_family
+    "update_user"
+  end
+
+  def container_definition
+    super.merge(
+      image: "k2nr/docker-user-manager",
+      mount_points: [
         {
-          name: "update_user",
-          cpu: 32,
-          memory: 32,
-          essential: true,
-          image: "k2nr/docker-user-manager",
-          mount_points: [
-            {
-              source_volume: "etc",
-              container_path: "/etc"
-            },
-            {
-              source_volume: "home",
-              container_path: "/home"
-            }
-          ]
-        }
-      ],
-      volumes: [
-        {
-          name: "etc",
-          host: {
-            source_path: "/etc"
-          }
+          source_volume: "etc",
+          container_path: "/etc"
         },
         {
-          name: "home",
-          host: {
-            source_path: "/home"
-          }
+          source_volume: "home",
+          container_path: "/home"
         }
       ]
     )
+  end
 
-    env = {
-      "USER_NAME" => user.name,
-      "USER_GROUPS" => user.instance_groups.join(",")
-    }
-    env["USER_PUBLIC_KEY"] = user.public_key if user.public_key.present?
-    env["USER_DOCKERCFG"] = section.dockercfg.to_json if section.dockercfg.present?
-
-    resp = aws.ecs.start_task(
-      cluster: section.cluster_name,
-      task_definition: "update_user",
-      overrides: {
-        container_overrides: [
-          {
-            name: "update_user",
-            environment: env.map { |k, v| {name: k, value: v} }
-          }
-        ]
+  def volumes
+    [
+      {
+        name: "etc",
+        host: {
+          source_path: "/etc"
+        }
       },
-      container_instances: section.container_instances.map{ |c| c[:container_instance_arn] }
-    )
+      {
+        name: "home",
+        host: {
+          source_path: "/home"
+        }
+      }
+    ]
   end
 end
