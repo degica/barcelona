a71e97bbb79891fbde7d65d30e0c08a812701ee4
Kazunori Kajihiro <likerichie@gmail.com>
2015-11-30 11:29:34 +0900

    Change to get heritage's env vars from task definition

--- app/serializers/heritage_serializer.rb
+++ app/serializers/heritage_serializer.rb
@@ -5,6 +5,10 @@ class HeritageSerializer < ActiveModel::Serializer
   belongs_to :district
 
   def env_vars
-    Hash[object.env_vars.map { |env| [env.key, env.value] }]
+    # Heritage#env_vars don't include env vars which are added by plugins
+    # as plugins add env vars to task_definition directly and those are not
+    # stored in DB'
+    task_definition = object.base_task_definition('_')
+    Hash[task_definition[:environment].map { |env| [env[:name], env[:value]] }]
   end
 end
