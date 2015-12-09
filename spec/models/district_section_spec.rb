ac017f37907bb9e00e3d59996db1689f60109857
Kazunori Kajihiro <likerichie@gmail.com>
2015-12-09 17:17:28 +0900

    Upload ecs.config even if dockercfg doesn't exist

--- spec/models/district_section_spec.rb
+++ spec/models/district_section_spec.rb
@@ -15,4 +15,19 @@ describe DistrictSection do
       it { expect(section.cluster_name).to eq district.name }
     end
   end
+
+  describe "#ecs_config" do
+    let(:name) { :private }
+    it { expect(section.send(:ecs_config)).to include "ECS_CLUSTER=#{section.cluster_name}" }
+    it { expect(section.send(:ecs_config)).to_not include "ECS_ENGINE_AUTH_DATA" }
+    it { expect(section.send(:ecs_config)).to_not include "ECS_ENGINE_AUTH_TYPE" }
+    it { expect(section.send(:ecs_config)).to include 'ECS_AVAILABLE_LOGGING_DRIVERS=["json-file", "syslog", "fluentd"]' }
+    it { expect(section.send(:ecs_config)).to include "ECS_RESERVED_MEMORY=128" }
+    context "when dockercfg exists" do
+      before do
+        district.dockercfg = {auth: "abcdef"}
+      end
+      it { expect(section.send(:ecs_config)).to_not include 'ECS_ENGINE_AUTH_DATA={"auth": "abcdef"}' }
+    end
+  end
 end
