3508ee666becf7e6e3f89c5f73543a8189e4a2ec
Kazunori Kajihiro <likerichie@gmail.com>
2015-12-24 11:30:48 +0900

    Log when deploy job fails

--- app/jobs/monitor_deployment_job.rb
+++ app/jobs/monitor_deployment_job.rb
@@ -6,7 +6,7 @@ class MonitorDeploymentJob < ActiveJob::Base
       service.heritage.events.create!(level: :good, message: "#{service.service_name}(#{service.heritage.image_path}) deployed")
     elsif count > 200
       # deploys not finished after 1000 seconds are marked as timeout
-      service.heritage.events.create!(level: :error, message: "Deploying #{service.service_name}(#{service.heritage.image_path}) timed out")
+      service.heritage.events.create!(level: :error, message: "Deploying #{service.service_name}(#{service.heritage.image_path}) has not finished for a while.")
     else
       MonitorDeploymentJob.set(wait: 5.seconds).perform_later(service, count: count + 1)
     end
