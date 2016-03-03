7292afc04958e896f905259a58ce351e48a450be
Kazunori Kajihiro <likerichie@gmail.com>
2016-03-03 17:33:26 +0900

    Unencrypt aws_access_key_id

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160202021528) do
+ActiveRecord::Schema.define(version: 20160303080907) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20160202021528) do
     t.string   "cluster_backend"
     t.integer  "cluster_size"
     t.string   "cluster_instance_type"
+    t.string   "aws_access_key_id"
   end
 
   create_table "env_vars", force: :cascade do |t|
