41beabaf411e4d8a8388c05f943f8639eec3933b
Kazunori Kajihiro <likerichie@gmail.com>
2016-03-10 16:44:30 +0900

    Fix index districts request spec

--- spec/requests/index_districts_spec.rb
+++ spec/requests/index_districts_spec.rb
@@ -3,10 +3,12 @@ require 'rails_helper'
 describe "GET /districts", type: :request do
   let(:user) { create :user }
   let(:auth) { {"X-Barcelona-Token" => user.token} }
-  let(:district) { create :district }
+  let!(:district) { create :district }
 
   it "lists districts" do
-    get "/v1/districts/#{district.name}", nil, auth
+    get "/v1/districts", nil, auth
     expect(response.status).to eq 200
+    districts = JSON.load(response.body)["districts"]
+    expect(districts.count).to eq 1
   end
 end
