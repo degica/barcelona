b385bf10356941f68fb05a3cdd8f890e0de351a9
Kazunori Kajihiro <likerichie@gmail.com>
2016-03-11 16:46:56 +0900

    Reverse releases order

--- app/models/heritage.rb
+++ app/models/heritage.rb
@@ -3,7 +3,7 @@ class Heritage < ActiveRecord::Base
   has_many :env_vars, dependent: :destroy
   has_many :oneoffs, dependent: :destroy
   has_many :events, dependent: :destroy
-  has_many :releases, dependent: :destroy, inverse_of: :heritage
+  has_many :releases, -> { order 'version DESC' }, dependent: :destroy, inverse_of: :heritage
   belongs_to :district, inverse_of: :heritages
 
   validates :name,
