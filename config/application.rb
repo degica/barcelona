12cd811454c86edd73cfa0e1baabfc975ecfa3e1
Kazunori Kajihiro <kazunori.kajihiro@gmail.com>
2016-07-01 17:50:37 +0900

    Resolve rails/rspec config deprecation

--- config/application.rb
+++ config/application.rb
@@ -31,9 +31,6 @@ module Barcelona
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
-
     config.middleware.insert_after ActionDispatch::DebugExceptions, ExceptionHandler
 
     config.active_job.queue_adapter = :delayed_job
