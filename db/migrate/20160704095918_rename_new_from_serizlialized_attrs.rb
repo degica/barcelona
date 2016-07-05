03e006a9d787be622cd107dc6f9275c1762eace6
Kazunori Kajihiro <kazunori.kajihiro@gmail.com>
2016-07-05 12:21:19 +0900

    Preserve old columns

--- db/migrate/20160704095918_rename_new_from_serizlialized_attrs.rb
+++ db/migrate/20160704095918_rename_new_from_serizlialized_attrs.rb
@@ -1,12 +1,12 @@
 class RenameNewFromSerizlializedAttrs < ActiveRecord::Migration
   def change
-    remove_column :services, :hosts
-    remove_column :services, :health_check
-
+    rename_column :services, :hosts, :old_hosts
     rename_column :services, :new_hosts, :hosts
+
+    rename_column :services, :health_check, :old_health_check
     rename_column :services, :new_health_check, :health_check
 
-    remove_column :plugins, :plugin_attributes
+    rename_column :plugins, :plugin_attributes, :old_plugin_attributes
     rename_column :plugins, :new_plugin_attributes, :plugin_attributes
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -97,10 +97,11 @@ ActiveRecord::Schema.define(version: 20160704095918) do
   add_index "oneoffs", ["heritage_id"], name: "index_oneoffs_on_heritage_id", using: :btree
 
   create_table "plugins", force: :cascade do |t|
+    t.text     "old_plugin_attributes"
     t.integer  "district_id"
     t.string   "name"
-    t.datetime "created_at",        null: false
-    t.datetime "updated_at",        null: false
+    t.datetime "created_at",            null: false
+    t.datetime "updated_at",            null: false
     t.text     "plugin_attributes"
   end
 
@@ -140,8 +141,10 @@ ActiveRecord::Schema.define(version: 20160704095918) do
     t.datetime "created_at",                              null: false
     t.datetime "updated_at",                              null: false
     t.string   "reverse_proxy_image"
+    t.text     "old_hosts"
     t.string   "service_type",        default: "default"
     t.boolean  "force_ssl"
+    t.text     "old_health_check"
     t.text     "hosts"
     t.text     "health_check"
   end
