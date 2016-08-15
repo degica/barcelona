66ce72feff1e184e7e24311d136aaec2f5a8e656
Kazunori Kajihiro <kazunori.kajihiro@gmail.com>
2016-08-16 07:34:51 +0900

    Add empty V2 ECS adapter

---
+++ app/models/backend/ecs/v2/adapter.rb
@@ -0,0 +1,19 @@
+module Backend::Ecs::V2
+  class Adapter
+    def initialize(service)
+      @service = service
+    end
+
+    def apply
+    end
+
+    def delete
+    end
+
+    def endpoint
+    end
+
+    def deployment_finished?
+    end
+  end
+end
