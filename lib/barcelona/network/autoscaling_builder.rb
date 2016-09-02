2321eba58f17dc3b59a9f986519a13545c6499a5
Kazunori Kajihiro <kazunori.kajihiro@gmail.com>
2016-09-02 13:42:11 +0900

    Update ECS optimized AMI

--- lib/barcelona/network/autoscaling_builder.rb
+++ lib/barcelona/network/autoscaling_builder.rb
@@ -10,7 +10,7 @@ module Barcelona
                      "ContainerInstanceLaunchConfiguration") do |j|
 
           j.IamInstanceProfile ref("ECSInstanceProfile")
-          j.ImageId "ami-058a4964" # amzn-ami-2016.03.g-amazon-ecs-optimized
+          j.ImageId "ami-2b6ba64a" # amzn-ami-2016.03.h-amazon-ecs-optimized
           j.InstanceType instance_type
           j.SecurityGroups [ref("InstanceSecurityGroup")]
           j.UserData instance_user_data
