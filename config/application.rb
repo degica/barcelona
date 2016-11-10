e49e37970c15e5edcb1499f0c5a6bdd4bbe7986f
Kazunori Kajihiro <kazunori.kajihiro@gmail.com>
2016-11-10 16:52:23 +0900

    API mode

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,8 @@ module Barcelona
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    config.api_only = true
+
     config.middleware.insert_after ActionDispatch::DebugExceptions, ExceptionHandler
 
     config.active_job.queue_adapter = :delayed_job
