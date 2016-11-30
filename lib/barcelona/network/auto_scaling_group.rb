7e540128e7775ce1457eccaef543eeea667afcaa
Kazunori Kajihiro <kazunori.kajihiro@gmail.com>
2016-11-30 11:39:16 -0800

    Extend rolling update timeout

--- lib/barcelona/network/auto_scaling_group.rb
+++ lib/barcelona/network/auto_scaling_group.rb
@@ -30,7 +30,7 @@ module Barcelona
             j.MaxBatchSize 1
             j.MinInstancesInService desired_capacity
             j.WaitOnResourceSignals true
-            j.PauseTime "PT20M"
+            j.PauseTime "PT60M"
           end
         end
       end
