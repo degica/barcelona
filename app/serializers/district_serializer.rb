cc4f2f9c76ac14f6b6d08696b223db46f6104735
Kazunori Kajihiro <kazunori.kajihiro@gmail.com>
2017-02-07 18:45:03 +0900

    Add aws_role to district serializer

--- app/serializers/district_serializer.rb
+++ app/serializers/district_serializer.rb
@@ -4,7 +4,7 @@ class DistrictSerializer < ActiveModel::Serializer
              :private_hosted_zone_id, :s3_bucket_name, :container_instances,
              :stack_status, :nat_type, :cluster_size, :cluster_instance_type,
              :cluster_backend, :cidr_block, :stack_name, :bastion_ip,
-             :aws_access_key_id
+             :aws_access_key_id, :aws_role
 
   has_many :heritages
   has_many :plugins
