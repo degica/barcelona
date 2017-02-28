f1a5e147d7aa2a983a9556cbef86b33e09e5894e
Kazunori Kajihiro <kazunori.kajihiro@gmail.com>
2017-02-28 15:08:39 +0900

    Simplify notification endpoint validation

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,14 +2,5 @@ class Notification < ApplicationRecord
   belongs_to :district, inverse_of: :notifications
 
   validates :target, presence: true, inclusion: {in: %w(slack)}
-  validate :validate_endpoint
-
-  def validate_endpoint
-    case target
-    when "slack"
-      unless endpoint =~ %r{^https://hooks\.slack\.com/services/}
-        errors.add(:notifications, "slack endpoint is invalid")
-      end
-    end
-  end
+  validates :endpoint, format: { with: %r{\Ahttps://hooks\.slack\.com/services/} }, if: proc { |n| n.target == "slack" }
 end
