b7795a40d90f108a1ac390033cb43f593ecca755
Kazunori Kajihiro <kazunori.kajihiro@gmail.com>
2017-04-11 11:19:31 +0900

    Add migration to set intermediate to existing endpoints

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170410092511) do
+ActiveRecord::Schema.define(version: 20170411021604) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
