b07aee56ebf498883ac7f83e8411b69248bc4c71
Kazunori Kajihiro <kazunori.kajihiro@gmail.com>
2017-05-17 12:18:32 +0900

    Add chaos_spec

---
+++ spec/lib/barcelona/chaos_spec.rb
@@ -0,0 +1,46 @@
+require "rails_helper"
+
+describe Barcelona::Chaos do
+  describe "#run" do
+    let(:district) { create :district }
+    let(:asg_mock) { double }
+    let(:chaos) { described_class.new(district, 1) }
+
+    before do
+      allow_any_instance_of(AwsAccessor).to receive(:autoscaling) { asg_mock }
+    end
+
+    context "when the district has only active instances" do
+      before do
+        expect(district).to receive(:container_instances) do
+          [
+            {status: 'ACTIVE', ec2_instance_id: 'i-11111111'}
+          ]
+        end
+      end
+
+      it "makes a terminate request for the instance" do
+        expect(asg_mock).to receive(:terminate_instance_in_auto_scaling_group).with(
+                              instance_id: 'i-11111111',
+                              should_decrement_desired_capacity: false
+                            )
+        chaos.run
+      end
+    end
+
+    context "when the district has non-active instance" do
+      before do
+        expect(district).to receive(:container_instances) do
+          [
+            {status: 'DRAINING', ec2_instance_id: 'i-11111111'}
+          ]
+        end
+      end
+
+      it "does not make a terminate request for the instance" do
+        expect(asg_mock).to_not receive(:terminate_instance_in_auto_scaling_group)
+        chaos.run
+      end
+    end
+  end
+end
