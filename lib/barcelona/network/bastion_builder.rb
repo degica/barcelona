9e7484c919371448373f8a5af098c4839578b0d3
Kazunori Kajihiro <kazunori.kajihiro@gmail.com>
2017-05-18 18:52:37 +0900

    Install awslogs to bastion server

--- lib/barcelona/network/bastion_builder.rb
+++ lib/barcelona/network/bastion_builder.rb
@@ -21,7 +21,49 @@ module Barcelona
       }
 
       def build_resources
+        add_resource("AWS::IAM::InstanceProfile", "BastionProfile") do |j|
+          j.Path "/"
+          j.Roles [ref("BastionRole")]
+        end
+
+        add_resource("AWS::IAM::Role", "BastionRole") do |j|
+          j.AssumeRolePolicyDocument do |j|
+            j.Version "2012-10-17"
+            j.Statement [
+              {
+                "Effect" => "Allow",
+                "Principal" => {
+                  "Service" => ["ec2.amazonaws.com"]
+                },
+                "Action" => ["sts:AssumeRole"]
+              }
+            ]
+          end
+          j.Path "/"
+          j.Policies [
+            {
+              "PolicyName" => "bastion-role",
+              "PolicyDocument" => {
+                "Version" => "2012-10-17",
+                "Statement" => [
+                  {
+                    "Effect" => "Allow",
+                    "Action" => [
+                      "logs:CreateLogGroup",
+                      "logs:CreateLogStream",
+                      "logs:DescribeLogStreams",
+                      "logs:PutLogEvents",
+                    ],
+                    "Resource" => ["*"]
+                  }
+                ]
+              }
+            }
+          ]
+        end
+
         add_resource("AWS::AutoScaling::LaunchConfiguration", "BastionLaunchConfiguration") do |j|
+          j.IamInstanceProfile ref("BastionProfile")
           j.ImageId AMI_IDS[district.region]
           j.InstanceType "t2.nano"
           j.SecurityGroups [ref("SecurityGroupBastion")]
@@ -36,10 +78,40 @@ module Barcelona
 
       def user_data
         ud = InstanceUserData.new
-        ud.packages += ["aws-cli", "yum-cron-security"]
+        ud.packages += ["aws-cli", "awslogs", "yum-cron-security"]
         ud.add_user("hopper")
         ud.add_file("/etc/ssh/ssh_ca_key.pub", "root:root", "644", district.ssh_format_ca_public_key)
+
+        log_group_name = "Barcelona/#{district.name}/bastion"
+        ud.add_file("/etc/awslogs/awslogs.conf", "root:root", "644", <<~EOS)
+          [general]
+          state_file = /var/lib/awslogs/agent-state
+
+          [/var/log/dmesg]
+          file = /var/log/dmesg
+          log_group_name = #{log_group_name}
+          log_stream_name = {ec2_id}/var/log/dmesg
+
+          [/var/log/messages]
+          file = /var/log/messages
+          log_group_name = #{log_group_name}
+          log_stream_name = {ec2_id}/var/log/messages
+          datetime_format = %b %d %H:%M:%S
+
+          [/var/log/secure]
+          file = /var/log/secure
+          log_group_name = #{log_group_name}
+          log_stream_name = {ec2_id}/var/log/secure
+          datetime_format = %b %d %H:%M:%S
+        EOS
+
         ud.run_commands += [
+          # awslogs
+          "ec2_id=$(curl http://169.254.169.254/latest/meta-data/instance-id)",
+          'sed -i -e "s/{ec2_id}/$ec2_id/g" /etc/awslogs/awslogs.conf',
+          'sed -i -e "s/us-east-1/'+district.region+'/g" /etc/awslogs/awscli.conf',
+          "service awslogs start",
+
           "service yum-cron start",
           'printf "\nTrustedUserCAKeys /etc/ssh/ssh_ca_key.pub\n" >> /etc/ssh/sshd_config',
           'sed -i -e "s/^PermitRootLogin .*/PermitRootLogin no/" /etc/ssh/sshd_config',
