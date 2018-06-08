4738448850fdcee20e9967684d22fe04253a03d8
Kazunori Kajihiro <kazunori.kajihiro@gmail.com>
2018-06-08 13:05:21 +0900

    Update docker-compose version

--- lib/barcelona/plugins/pcidss_plugin.rb
+++ lib/barcelona/plugins/pcidss_plugin.rb
@@ -97,7 +97,7 @@ module Barcelona
           # Start/Install docker and compose
           "service docker start",
           "usermod -a -G docker ec2-user",
-          "curl -L https://github.com/docker/compose/releases/download/1.14.0/docker-compose-Linux-x86_64 > /usr/local/bin/docker-compose",
+          "curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-Linux-x86_64 > /usr/local/bin/docker-compose",
           "chmod +x /usr/local/bin/docker-compose",
 
           # Setup OSSEC manager
