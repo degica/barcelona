fa36de0e245f983857c485b980300c516e7d6e4f
Kazunori Kajihiro <kazunori.kajihiro@gmail.com>
2019-03-01 11:06:31 +0900

    Add description

--- app/models/environment.rb
+++ app/models/environment.rb
@@ -9,7 +9,12 @@ class Environment < ApplicationRecord
   validates :value, presence: true, if: -> { value_from.blank? }
   validates :value_from, presence: true, if: -> { value.blank? }
 
+
+  # If value_from is nil return it as-is
+  # if value_from is a relative name prefix /barcelona/district-name/
+  #   ECS resolves relative value_from as a SSM parameter in the same account and region
   def namespaced_value_from
+    return nil if value_from.nil?
     return value_from if value_from.start_with?("arn:aws:")
 
     s = value_from
