3f439336c10ac934f9814a9c3609717f3b81e4d0
Taku Nakajima <tnakajima@degica.com>
2019-11-14 19:01:19 +0900

    Refactor new Plugins

--- spec/lib/barcelona/plugins/ossec_client_plugin_spec.rb
+++ spec/lib/barcelona/plugins/ossec_client_plugin_spec.rb
@@ -16,25 +16,38 @@ module Barcelona
                ]
       end
 
-      context "gets hooked with container_instance_user_data trigger" do
+      shared_examples_for('ossec client') do
+        it "installs wazuh client" do
+          expect(user_data["packages"]).to include('wazuh-agent')
+        end
+
+        it "configures wazuh client propperly" do
+          expect(user_data["runcmd"]).to include("/var/ossec/bin/agent-auth -m ossec-manager.test.local")
+          expect(user_data["runcmd"]).to include("/var/ossec/bin/ossec-control restart")
+        end
+      end
+
+      context "when hooked with container_instance_user_data trigger" do
         before do
           district.save!
         end
         let(:ci) { ContainerInstance.new(district) }
-        subject {YAML.load(Base64.decode64(ci.user_data.build)) } 
+        let(:user_data) {YAML.load(Base64.decode64(ci.user_data.build)) } 
 
-        it "installs wazuh client" do
-          expect(subject["packages"]).to include('wazuh-agent')
+        it_behaves_like('ossec client')
+      end
+
+      context "when hooked with network_stack_template trigger" do
+        before do
+          district.save!
+        end
+        let(:user_data) do
+          template = JSON.load(::Barcelona::Network::NetworkStack.new(district).target!)
+          user_data_base64 = template["Resources"]["BastionLaunchConfiguration"]["Properties"]["UserData"]
+          YAML.load(Base64.decode64(user_data_base64))
         end
 
-        it "configures wazuh client propperly" do
-          ossec_conf = subject['write_files'].find do |f|
-            f['path'] == '/var/ossec/etc/ossec.conf'
-          end
-          expect(ossec_conf['content']).to match %r[<server-hostname>ossec-manager.test.local</server-hostname>]
-          expect(subject["runcmd"]).to include("/var/ossec/bin/agent-auth -m ossec-manager.test.local")
-          expect(subject["runcmd"]).to include("/var/ossec/bin/ossec-control restart")
-        end
+        it_behaves_like('ossec client')
       end
     end
   end
