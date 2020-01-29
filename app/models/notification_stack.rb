da65df87ee07f60f653dd8365ecac0226efd597c
Taku Nakajima <tnakajima@degica.com>
2020-01-29 11:53:25 +0900

    Upgrade nodejs runtime to 12.x

--- app/models/notification_stack.rb
+++ app/models/notification_stack.rb
@@ -64,7 +64,7 @@ class NotificationStack < CloudFormation::Stack
                 j.DISTRICT stack.district.name
               end
             end
-            j.Runtime "nodejs8.10"
+            j.Runtime "nodejs12.x"
             j.Code do |j|
               j.ZipFile slack_notification_code
             end
