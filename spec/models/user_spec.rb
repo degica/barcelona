3eb8ebaefe3a51dcb185631398a64c7d36a1adde
David Siaw <dsiaw@degica.com>
2021-01-15 13:26:38 +0900

    user-validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe User do
   it { is_expected.to validate_presence_of(:name) }
-  it { is_expected.to validate_uniqueness_of(:name) }
+  it { is_expected.to validate_uniqueness_of(:name).scoped_to(:auth) }
 
   describe '#token' do
     it 'does not save the actual token' do
