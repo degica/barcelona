a29189515cec43ee688f371c151b6fb13b1be68c
Yuki Osaki <yuki.osaki7@gmail.com>
2021-06-08 14:40:38 +0900

    add codes to chceck if ssm client is called

--- spec/services/ssm_parameters_spec.rb
+++ spec/services/ssm_parameters_spec.rb
@@ -14,6 +14,8 @@ describe SsmParameters do
     let(:parameter_value) { "test123"}
 
     it "put ssm parameters" do
+      expect_any_instance_of(Aws::SSM::Client).to receive(:put_parameter).and_call_original
+
       response = described_class.new(district, name).put_parameter(parameter_value)
       expect(response.version).to eq 0
     end
@@ -21,6 +23,7 @@ describe SsmParameters do
 
   describe "#delete_parameter" do
     it "delete ssm parameter" do
+      expect_any_instance_of(Aws::SSM::Client).to receive(:delete_parameters).and_call_original
       response = described_class.new(district, name).delete_parameter
       expect(response.deleted_parameters).to eq []
       expect(response.invalid_parameters).to eq []
